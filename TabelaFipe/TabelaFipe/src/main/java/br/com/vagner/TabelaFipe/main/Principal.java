b42d5cb35742fa1bb35c301bddc525c3b8e8bc66
VagnerSousa <vagner.s.sousa@outlook.com>
2024-11-28 21:49:18 -0300

    implementando a busca por modelos

--- TabelaFipe/TabelaFipe/src/main/java/br/com/vagner/TabelaFipe/main/Principal.java
+++ TabelaFipe/TabelaFipe/src/main/java/br/com/vagner/TabelaFipe/main/Principal.java
@@ -1,13 +1,18 @@
 package br.com.vagner.TabelaFipe.main;
 
+import br.com.vagner.TabelaFipe.model.Dados;
+import br.com.vagner.TabelaFipe.model.Modelos;
 import br.com.vagner.TabelaFipe.service.ConsumoApi;
+import br.com.vagner.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
     Scanner leitura = new Scanner(System.in);
     private final String URL_BASE ="https://parallelum.com.br/fipe/api/v1/";
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
 
 
     public void exibirMenu(){
@@ -35,6 +40,24 @@ public class Principal {
 
         var json = consumo.obterDados(endereco);
         System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para consulta");
+
+        var codigoMarca = leitura.nextLine();
+        endereco = endereco +"/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
 
 
     }
